55bbfece2b95177c397fac537b486bb1bfe89938
waynelu92 <waynelu92@gmail.com>
2016-09-15 22:43:58 +0800

    Second commit

--- app/src/main/java/com/bignerdranch/android/nerdlauncher/NerdLauncherActivity.java
+++ app/src/main/java/com/bignerdranch/android/nerdlauncher/NerdLauncherActivity.java
@@ -1,8 +1,6 @@
 package com.bignerdranch.android.nerdlauncher;
 
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
 public class NerdLauncherActivity extends SingleFragmentActivity {
 
